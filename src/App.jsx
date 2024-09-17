ceae1ae31e9a5129ce3dade429442c0facd96011
Chef <cheftech@patraldo.com>
2024-09-17 14:09:53 -0600

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Solid
+	  Mexican Bold - Orgullosamente Indiscreto
         </a>
       </header>
     </div>
